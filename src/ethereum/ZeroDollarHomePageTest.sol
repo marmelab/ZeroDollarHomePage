d23474594a3574adcaeb891df513af2231d9cb0c
djhi <gildas.garcia@gmail.com>
2016-02-23 17:41:27 +0100

    Refactoring: only have one getLastNonPublished function now

--- src/ethereum/ZeroDollarHomePageTest.sol
+++ src/ethereum/ZeroDollarHomePageTest.sol
@@ -13,7 +13,6 @@ contract ZeroDollarHomePageTest is Asserter {
         app.newPullRequest("c603fa2eff16a0ecec85cf00aee0b6074be0c92d", "toto@toto.com", "toto");
         var result = app.claimWithImage("c603fa2eff16a0ecec85cf00aee0b6074be0c92d", "http://google.com/image.jpg");
         assertUintsEqual(result, uint8(ZeroDollarHomePage.ResponseCodes.Ok), "Should have returned Ok");
-        assertUintsEqual(app.queueLength(), 1, "Should have returned 1");
     }
 
     function test_closeRequest_should_returns_Ok() {
@@ -23,7 +22,19 @@ contract ZeroDollarHomePageTest is Asserter {
 
         var result = app.closeRequest("c603fa2eff16a0ecec85cf00aee0b6074be0c92d");
         assertUintsEqual(result, uint8(ZeroDollarHomePage.ResponseCodes.Ok), "Should have returned Ok");
-        assertUintsEqual(app.queueLength(), 0, "Should have returned 0");
+    }
+
+    function test_getLastNonPublished_should_returns_the_request() {
+        ZeroDollarHomePage app = new ZeroDollarHomePage();
+        app.newPullRequest("c603fa2eff16a0ecec85cf00aee0b6074be0c92d", "toto@toto.com", "toto");
+        app.claimWithImage("c603fa2eff16a0ecec85cf00aee0b6074be0c92d", "http://google.com/image.jpg");
+        app.newPullRequest("c603fa2eff16a0ecec85cf00aee0b6074be0c92e", "toto2@toto2.com", "toto2");
+        app.claimWithImage("c603fa2eff16a0ecec85cf00aee0b6074be0c92e", "http://google.com/image2.jpg");
+        var (id,,) = app.getLastNonPublished();
+        assertUintsEqual(id, 1, "Should have returned id 1");
+        app.closeRequest("c603fa2eff16a0ecec85cf00aee0b6074be0c92d");
+        (id,,) = app.getLastNonPublished();
+        assertUintsEqual(id, 2, "Should have returned id 2");
     }
 
 }
