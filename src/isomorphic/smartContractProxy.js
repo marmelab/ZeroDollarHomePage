5b729bef2c7ad4ae6af9ba8cd2298f71ee6f524e
Kmaschta <kevin@marmelab.com>
2016-02-25 10:52:53 +0100

    Clean smartContractProxy

--- src/isomorphic/smartContractProxy.js
+++ src/isomorphic/smartContractProxy.js
@@ -2,11 +2,49 @@ import fs from 'fs';
 import eris from 'eris-contracts';
 import contractData from '../ethereum/epm.json';
 
-export default function smartContractFactory(name, {url, account}) {
+export const cleanContractValue = entry => {
+    if (entry.c) return entry.c[0];
+    return entry;
+};
+
+export function proxifyFunction(replacedFunction) {
+    return (...args) => {
+        return new Promise((resolve, reject) => {
+            args.push((err, value) => {
+                if (err) return reject(err);
+                if (Array.isArray(value)) return resolve(value.filter(v => !!v).map(cleanContractValue));
+                return resolve(cleanContractValue(value));
+            });
+            replacedFunction(...args);
+        });
+    };
+}
+
+/**
+ * Usage:
+ *      const mySmartContract = smartContractProxy('MySmartContract', {
+ *          url: 'http://localhost:1337/rpc',
+ *          account: {...yourAccount},
+ *      });
+ *      yield mySmartContract.setAttribute('new value');
+ *      const result = yield mySmartContract.getAttribute();
+ */
+export default function smartContractFactory(name, {url, account}, excludedFields = []) {
     const address = contractData[`deploy${name}`];
     const abi = JSON.parse(fs.readFileSync(`./src/ethereum/abi/${address}`));
     const manager = eris.newContractManagerDev(url, account);
     const contract = manager.newContractFactory(abi).at(address);
+
+    // Exclude default functions
+    ['getContributorToDisplay'].forEach(field => excludedFields.push(field));
+
+    contract.abi
+        .filter(field => field.name && excludedFields.indexOf(field.name) === -1)
+        .forEach(field => {
+            if (field.type === 'function') {
+                contract[field.name] = proxifyFunction(contract[field.name]);
+            }
+        });
 
     return contract;
 }
