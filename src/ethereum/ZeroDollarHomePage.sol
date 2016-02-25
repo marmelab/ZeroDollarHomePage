8ecece6cb3e7410b16f016260eca03fa666b9fed
djhi <gildas.garcia@gmail.com>
2016-02-25 10:29:46 +0100

    [RFR] Refactored solidity contracts
    We now have only 3 methods:
    - [x] `newRequest` registers a new pull request with its key as id along with author name and image url. It returns the publication date.
    - [x] `closeRequest` marks the request as having been display and remove it from queue
    - [x] `getLastNonPublished` returns the next request to be published as [`id`, `authorName`, `imageUrl`, `createdAt`]
    - [x] All functions returns a response code as their first parameter

--- src/ethereum/ZeroDollarHomePage.sol
+++ src/ethereum/ZeroDollarHomePage.sol
@@ -3,27 +3,25 @@ contract ZeroDollarHomePage {
 
     enum ResponseCodes {
         Ok,
-        InvalidSha,
+        InvalidPullRequestId,
         InvalidAuthorEmail,
         InvalidAuthorName,
         InvalidImageUrl,
-        RequestNotFound
+        RequestNotFound,
+        EmptyQueue
     }
 
     struct Request {
         uint id;
-        string commitSha;
-        string authorEmail;
         string authorName;
         string imageUrl;
         uint createdAt; // timestamp for pull request creation
-        uint claimedAt; // timestamp for image submission
         uint displayedAt; // timestamp for image display
     }
 
-    mapping (string => Request) _requests; // key is the commit sha
+    mapping (uint => Request) _requests; // key is the pull request id
     uint public numberOfRequests;
-    string[] _queue;
+    uint[] _queue;
     uint public queueLength;
     uint _current;
 
@@ -36,67 +34,48 @@ contract ZeroDollarHomePage {
     /*
      * Register a new pull request merged on github
      */
-    function newPullRequest(string commitSha, string authorEmail, string authorName) returns (uint8) {
-        if (bytes(commitSha).length != ShaLength) {
-            return uint8(ResponseCodes.InvalidSha);
-        }
-
-        // TODO: better validation of emails
-        if (bytes(authorEmail).length <= 0) {
-            return uint8(ResponseCodes.InvalidAuthorEmail);
+    function newRequest(uint pullRequestId, string authorName, string imageUrl) returns (uint8 code, uint displayDate) {
+        if (pullRequestId <= 0) {
+            code = uint8(ResponseCodes.InvalidPullRequestId);
+            return;
         }
 
         if (bytes(authorName).length <= 0) {
-            return uint8(ResponseCodes.InvalidAuthorName);
-        }
-
-        numberOfRequests += 1;
-        _requests[commitSha].id = numberOfRequests;
-        _requests[commitSha].commitSha = commitSha;
-        _requests[commitSha].authorEmail = authorEmail;
-        _requests[commitSha].authorName = authorName;
-        _requests[commitSha].createdAt = now;
-        return uint8(ResponseCodes.Ok);
-    }
-
-    /*
-     * Saves the image submitted by a pull request author
-     */
-    function claimWithImage(string commitSha, string imageUrl) returns (uint8) {
-        if (bytes(commitSha).length != ShaLength) {
-            return uint8(ResponseCodes.InvalidSha);
+            code = uint8(ResponseCodes.InvalidAuthorName);
+            return;
         }
 
         if (bytes(imageUrl).length <= 0) {
-            return uint8(ResponseCodes.InvalidImageUrl);
+            code = uint8(ResponseCodes.InvalidImageUrl);
+            return;
         }
 
-        if (bytes(_requests[commitSha].commitSha).length == 0) {
-            return uint8(ResponseCodes.RequestNotFound);
-        }
+        numberOfRequests += 1;
+        _requests[pullRequestId].id = pullRequestId;
+        _requests[pullRequestId].authorName = authorName;
+        _requests[pullRequestId].imageUrl = imageUrl;
+        _requests[pullRequestId].createdAt = now;
 
-        _requests[commitSha].imageUrl = imageUrl;
-        _requests[commitSha].claimedAt = now;
-
-        _queue.push(commitSha);
+        _queue.push(pullRequestId);
         queueLength += 1;
 
-        return uint8(ResponseCodes.Ok);
+        code = uint8(ResponseCodes.Ok);
+        displayDate = now + (queueLength * 1 days);
     }
 
     /*
      * Marke a pull request with its image has having been displayed.
      */
-    function closeRequest(string commitSha) returns (uint8) {
-        if (bytes(commitSha).length != ShaLength) {
-            return uint8(ResponseCodes.InvalidSha);
+    function closeRequest(uint pullRequestId) returns (uint8) {
+        if (pullRequestId <= 0) {
+            return uint8(ResponseCodes.InvalidPullRequestId);
         }
 
-        if (bytes(_requests[commitSha].commitSha).length == 0) {
+        if (_requests[pullRequestId].id <= 0) {
             return uint8(ResponseCodes.RequestNotFound);
         }
 
-        _requests[commitSha].displayedAt = now;
+        _requests[pullRequestId].displayedAt = now;
         delete _queue[0];
         queueLength -= 1;
         _current = _current + 1;
@@ -106,18 +85,16 @@ contract ZeroDollarHomePage {
     /*
      * Get the next image to be displayed on the ZeroDollarHomePage site
      */
-    function getLastNonPublished() returns (uint id, string commitSha, string authorEmail, string authorName, string imageUrl, uint createdAt, uint claimedAt) {
+    function getLastNonPublished() returns (uint8 code, uint id, string authorName, string imageUrl, uint createdAt) {
         if (_queue.length == 0) {
-            return;
+            code = uint8(ResponseCodes.EmptyQueue);
         }
 
         var request = _requests[_queue[_current]];
         id = request.id;
-        commitSha = request.commitSha;
-        authorEmail = request.authorEmail;
         authorName = request.authorName;
         imageUrl = request.imageUrl;
         createdAt = request.createdAt;
-        claimedAt = request.claimedAt;
+        code = uint8(ResponseCodes.Ok);
     }
 }
