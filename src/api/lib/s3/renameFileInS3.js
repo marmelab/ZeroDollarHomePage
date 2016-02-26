4f9b3dee3fef1b3c19aa9438119cbef6a32c6348
djhi <gildas.garcia@gmail.com>
2016-02-26 15:57:54 +0100

    Review

--- src/api/lib/s3/renameFileInS3.js
+++ src/api/lib/s3/renameFileInS3.js
@@ -3,8 +3,10 @@ import moment from 'moment';
 
 export const renameFileInS3 = (client, config) => fileUrl => new Promise((resolve, reject) => {
     const headers = {
+        'x-amz-metadata-directive': 'REPLACE',
         'Content-Type': 'image/jpeg',
         'x-amz-acl': 'public-read',
+        'x-amz-meta-Cache-Control': 'max-age=86400',
         'Expires': moment().endOf('day').utc().toISOString(),
     };
 
